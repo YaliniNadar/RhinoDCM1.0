cb72e409f5a6eb7f60a8d1f860cc54483ff286c3
YaliniNadar <you@example.com>
2024-02-07 20:09:58 -0500

    added ShinyStorage to save data into IndexedDB and retrieve the values

--- app/js/index.js
+++ app/js/index.js
@@ -2,16 +2,70 @@ export function goBack() {
     window.history.back();
 }
 
-export function saveToLocalStorage(data) {
-    for (var key in data) {
-        if (data.hasOwnProperty(key)) {
-            localStorage.setItem(key, data[key]);
+function updateUI(inputId, value) {
+    var element = document.getElementById(inputId);
+    console.log(inputId);
+    if (element) {
+        var inputType = element.type;
+        switch (inputType) {
+            case 'checkbox':
+            case 'radio':
+                element.checked = value; // Set 'value' as boolean
+                break;
+            case 'select-multiple':
+                // Handle multiple selection for select elements
+                // Example: Set selected options based on 'value'
+                break;
+            default:
+                element.value = value; // Update value attribute for text inputs, textareas, etc.
+                console.log("updated value on UI")
+                break;
         }
+        // Pass the value to Shiny
+        Shiny.setInputValue(inputId, value);
+    } else {
+        console.warn('Element with ID', inputId, 'not found or inaccessible.');
     }
 }
 
-export function getDefaultValue(key) {
-    var storedValue = localStorage.getItem(key);
-    console.log(storedValue);
+export function readIndexedDBAndSave(dbName, inputIds, prefix) {
+    var request = indexedDB.open(dbName);
+
+    // Event handler for errors during IndexedDB opening
+    request.onerror = function(event) {
+        console.error('Error opening IndexedDB:', event.target.error);
+    };
+
+    // Event handler for success of IndexedDB opening
+    request.onsuccess = function(event) {
+        var db = event.target.result;
+        console.log('IndexedDB:', db);
+
+        var transaction = db.transaction('shinyStoresPlus', 'readonly');
+        console.log('Database opened successfully');
+
+        var objectStore = transaction.objectStore('shinyStoresPlus');
+        console.log('Object store retrieved:', objectStore);
+
+        var index = objectStore.index('var');
+
+        for (var i = 0; i < inputIds.length; i++) {
+            (function(inputId) { // Using an IIFE to create a closure
+                var getRequest = index.get(prefix + inputId);
+                getRequest.onsuccess = function(event) {
+                    var result = event.target.result;
+                    if (result) {
+                        console.log('Found object for input ID', inputId, ':', result);
+                        var value = result.value;
+                        console.log('Value:', value);
+                        updateUI(prefix + inputId, value);
+                    } else {
+                        console.log('No matching object found for input ID', prefix + inputId);
+                    }
+                };
+            })(inputIds[i]); // Pass the current value of inputId into the IIFE
+        }
+    };
 }
+
   
--- app/view/param_specs.R
+++ app/view/param_specs.R
@@ -5,7 +5,6 @@ box::use(
         h2,
         p,
         br,
-        tags,
         HTML,
         numericInput,
         textInput,
@@ -16,6 +15,7 @@ box::use(
         renderUI,
         observe],
   shinyjs[useShinyjs, runjs],
+  shinyStorePlus[initStore, setupStorage],
 )
 
 box::use(
@@ -27,6 +27,7 @@ ui <- function(id) {
   ns <- NS(id)
 
   fluidPage(
+    initStore(),
     useShinyjs(),
     h2("Parameter Specifications"),
     br(),
@@ -45,59 +46,6 @@ ui <- function(id) {
                  choices = c("Yes", "No"), selected = NULL),
 
     uiOutput(ns("conditional_num_items")),
-
-    tags$script(
-      '
-      // Beforeunload event to store values in localStorage before refresh
-      window.addEventListener("beforeunload", function() {
-        console.log("Beforeunload event triggered");
-        var numTimePoints = $("#num_time_points").val();
-        var numAttributes = $("#num_attributes").val();
-        var attributeNames = $("#attribute_names").val();
-        var qMatrixChoice = $("input[name=q_matrix_choice]:checked").val();
-
-        // Create an object with the values
-        var valuesToSave = {
-          num_time_points: num_time_points,
-          num_attributes: num_attributes,
-          attribute_names: attribute_names,
-          q_matrix_choice: q_matrix_choice
-        };
-
-        // Convert the object to a JSON string
-        var valuesJSON = JSON.stringify(valuesToSave);
-
-        // Save the JSON string to local storage
-        localStorage.setItem("saved_values", valuesJSON);
-      });
-
-      $(document).ready(function() {
-        console.log("Getting values from localStorage on document ready");
-        console.log("num_time_points:", localStorage.getItem("num_time_points"));
-        console.log("num_attributes:", localStorage.getItem("num_attributes"));
-        console.log("attribute_names:", localStorage.getItem("attribute_names"));
-
-        // Read data from local storage
-        var numTimePoints = localStorage.getItem("num_time_points");
-        var numAttributes = localStorage.getItem("num_attributes");
-        var attributeNames = localStorage.getItem("attribute_names");
-        var qMatrixChoice = localStorage.getItem("q_matrix_choice");
-
-        // Update UI elements with retrieved data
-        $("#num_time_points").val(numTimePoints);
-        console.log($("#num_time_points").val())
-        $("#num_attributes").val(numAttributes);
-        $("#attribute_names").val(attributeNames);
-
-        if (qMatrixChoice !== null) {
-          $("input[name=q_matrix_choice][value=" + qMatrixChoice + "]").prop("checked", true);
-        }
-        
-      });
-      '
-    ),
-
-    # tags$script(src = "app/js/app.js"),
 
     ui_components$next_button(ns("nextButton")),
     ui_components$back_button(ns("backButton")),
@@ -120,17 +68,14 @@ server <- function(id) {
       }
     })
 
-    # Save data to local storage
+    # Read from Indexed DB and Save Input Values
     observe({
-      values_to_save <- list(
-        num_time_points = input$num_time_points,
-        num_attributes = input$num_attributes,
-        attribute_names = input$attribute_names,
-        q_matrix_choice = input$q_matrix_choice
-      )
-      values_json <- jsonlite::toJSON(values_to_save, auto_unbox = TRUE)
-      runjs(paste("App.saveToLocalStorage(", values_json, ");"))
+      runjs("App.readIndexedDBAndSave(Sys.getenv('DB_NAME'),['num_time_points', 'num_attributes', 'attribute_names', 'q_matrix_choice'], 'app-param_specs-')")
     })
+
+    # insert at the bottom  !!!IMPORTANT
+    appid <- Sys.getenv("APP_ID")
+    setupStorage(appId = appid, inputs = TRUE)
 
     ui_components$nb_server("nextButton", "q_matrix")
 
