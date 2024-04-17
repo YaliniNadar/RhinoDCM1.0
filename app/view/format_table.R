e49a57beb132ee48ba5ff3f1c5388781a1308dc8
YaliniNadar <you@example.com>
2024-04-17 12:52:15 -0400

    added plot functionality

--- app/view/format_table.R
+++ app/view/format_table.R
@@ -3,19 +3,6 @@ box::use(
 )
 
 #' @export
-get_formatted_table <- function(table, columns, digits = 3) {
-  formatted_table <- formatRound(
-    table,
-    columns = columns,
-    digits = digits
-  )
-
-  print("HELOOOOOOOO")
-  print(typeof(datatable(formatted_table)))
-  return(datatable(formatted_table))
-}
-
-#' @export
 format_pagination <- function() {
   jquery_code <- "function(settings, json) {$(this.api().table().container()).find('.dataTables_paginate').css({'background-color': '#202020', 'color': '#fff'});}" # nolint
   return(jquery_code)
